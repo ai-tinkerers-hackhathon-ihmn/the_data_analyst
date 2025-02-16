6d75df34addf9710f01f3221a66a76956cd3a7df
VianneyMI <vianney.mixtur@outlook.fr>
2025-02-16 12:35:14 +0100

    Fix errors in implementation of the agents

--- backend/src/backend/agents/query_analyzer.py
+++ backend/src/backend/agents/query_analyzer.py
@@ -1,8 +1,19 @@
 """`backend.agents.query_analyzer` module."""
 
-from smolagents import CodeAgent
+from smolagents import CodeAgent, LiteLLMModel
+
+
+MODEL_ID: str = "anthropic/claude-3-5-sonnet-latest"
+
+llm = LiteLLMModel(
+    model_id=MODEL_ID,
+)
+
 
 query_analyzer = CodeAgent(
+    model=llm,
     name="query_analyzer",
     description="Analyzes user queries and retrieves the necessary data from the database.",
+    tools = [],
+    add_base_tools = True
 )
