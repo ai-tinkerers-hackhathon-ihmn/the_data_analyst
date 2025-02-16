55ef58b4f32bf56308b7ca65c9223766d35bebea
VianneyMI <vianney.mixtur@outlook.fr>
2025-02-16 12:35:23 +0100

    Test the agent through gradio

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -3,9 +3,18 @@
 Entry point for the backend.
 """
 
+from smolagents import GradioUI
+from backend.agents.orchestrator import orchestrator
+from backend.setup import setup
+
 def main()->None:
     """Entry point for the backend."""
     print("Hello, world!")
+    setup()
+    ui = GradioUI(orchestrator)
+    ui.launch()
+
+
 
 
 if __name__ == "__main__":
