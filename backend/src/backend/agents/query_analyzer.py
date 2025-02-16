3e55f023df74db9b7f12a08f6ef54f0d11d5d41d
Carole Ibrahim <carole@Caroles-MacBook-Pro.local>
2025-02-16 13:47:28 +0100

    postgre agent and tool

--- backend/src/backend/agents/query_analyzer.py
+++ backend/src/backend/agents/query_analyzer.py
@@ -1,8 +1,18 @@
 """`backend.agents.query_analyzer` module."""
 
-from smolagents import CodeAgent
+from smolagents import CodeAgent, LiteLLMModel
+
+## import relevant tools
+import sys
+sys.path.append('../')
+from tools.postgre_tool import PostgresQueryTool
+
+model = LiteLLMModel(model_id="anthropic/claude-3-5-sonnet-latest", 
+                     api_key="YOUR_ANTHROPIC_API_KEY") 
 
 query_analyzer = CodeAgent(
     name="query_analyzer",
     description="Analyzes user queries and retrieves the necessary data from the database.",
+    tools = [PostgresQueryTool()],
+    model = model
 )
